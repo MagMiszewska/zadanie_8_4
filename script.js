d3e086ee12c064ddcc457b581d610e8f20eb6d02
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-04-03 15:38:21 +0200

    Add files

---
+++ script.js
@@ -0,0 +1,11 @@
+var name = prompt('Wpisz swoje imię'); // prompt dialog box
+alert('Jak się masz ' + name + '?'); //alert dialog box
+
+if(confirm('Chcesz kontynuować?')){ //confirm dialog box
+    alert('Śmiało!');
+} else{
+    alert('Pa pa');
+}
+/*
+console.log('Jak się masz ' + name + '?');
+*/
